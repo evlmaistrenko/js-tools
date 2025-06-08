5fed731ef05ba7e970c40c1dd28bd158081ec6b5
evlmaistrenko <evlmaistrenko@users.noreply.github.com>
2025-06-08 15:51:59 +0000

    deploy: 7740b0dce55ba77c6e66114199b97fb772744c77

---
+++ 7740b0dce55ba77c6e66114199b97fb772744c77/react/storybook/assets/Introduction-VUYFzfWX.js
@@ -0,0 +1,24 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-C4yEQdft.js";import{c as r,e as a}from"./blocks-BZl9nACA.js";import"./iframe-DQsUR6bV.js";import"./index-B_DOrijS.js";const i=`# @evlmaistrenko/tools-react
+
+[![NPM Version](https://img.shields.io/npm/v/%40evlmaistrenko%2Ftools-react)](https://www.npmjs.com/package/@evlmaistrenko/tools-react)
+
+Useful react-components.
+
+## Usage
+
+\`\`\`bash
+npm i @evlmaistrenko/tools-react
+\`\`\`
+
+\`\`\`javascript
+import { WithOverlay } from "@evlmaistrenko/tools-react";
+
+const withOverlay = <WithOverlay />;
+\`\`\`
+
+## Api docs
+
+- [Typedoc](https://evlmaistrenko.github.io/js-tools/react/typedoc/)
+- [Storybook](https://evlmaistrenko.github.io/js-tools/react/storybook/)
+`;function e(o){return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Introduction"}),`
+`,t.jsx(a,{children:i})]})}function d(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e()}export{d as default};
