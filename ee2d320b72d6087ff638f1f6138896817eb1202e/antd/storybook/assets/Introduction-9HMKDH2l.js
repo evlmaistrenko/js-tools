747c83f052cb6481c6a3ab523103bc6081d9ffa7
evlmaistrenko <evlmaistrenko@users.noreply.github.com>
2025-06-11 08:53:13 +0000

    deploy: ee2d320b72d6087ff638f1f6138896817eb1202e

---
+++ ee2d320b72d6087ff638f1f6138896817eb1202e/antd/storybook/assets/Introduction-9HMKDH2l.js
@@ -0,0 +1,25 @@
+import{j as n,b as s,c as a}from"./blocks-CBZhx3G0.js";import{useMDXComponents as r}from"./index-FYYGCfDY.js";import"./iframe-DjQgHL-s.js";const i=`# @evlmaistrenko/tools-antd
+
+[![NPM Version](https://img.shields.io/npm/v/%40evlmaistrenko%2Ftools-antd)](https://www.npmjs.com/package/@evlmaistrenko/tools-antd)
+
+React-components based on Ant Design.
+
+## Usage
+
+\`\`\`bash
+npm i @evlmaistrenko/tools-antd
+\`\`\`
+
+\`\`\`javascript
+import { TelegramOutlined } from "@evlmaistrenko/tools-antd";
+
+const telegramOutlined = <TelegramOutlined />;
+\`\`\`
+
+## Api docs
+
+- [Typedoc](https://evlmaistrenko.github.io/js-tools/antd/typedoc/)
+- [Storybook](https://evlmaistrenko.github.io/js-tools/antd/storybook/)
+- [CHANGELOG.md](./CHANGELOG.md)
+`;function e(t){return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Introduction"}),`
+`,n.jsx(a,{children:i})]})}function p(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e()}export{p as default};
---
+++ ee2d320b72d6087ff638f1f6138896817eb1202e/react/storybook/assets/Introduction-CmbPuIuG.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-37248ZA3.js";import{b as r,c as a}from"./blocks-CX6_Orju.js";import"./iframe-CpBeUX4E.js";import"./index-CWUcqQxO.js";const i=`# @evlmaistrenko/tools-react
+
+[![NPM Version](https://img.shields.io/npm/v/%40evlmaistrenko%2Ftools-react)](https://www.npmjs.com/package/@evlmaistrenko/tools-react)
+
+Useful react-components.
+
+## Usage
+
+\`\`\`bash
+npm i @evlmaistrenko/tools-react
+\`\`\`
+
+\`\`\`javascript
+import { WithOverlay } from "@evlmaistrenko/tools-react";
+
+const withOverlay = <WithOverlay />;
+\`\`\`
+
+## Api docs
+
+- [Typedoc](https://evlmaistrenko.github.io/js-tools/react/typedoc/)
+- [Storybook](https://evlmaistrenko.github.io/js-tools/react/storybook/)
+- [CHANGELOG.md](./CHANGELOG.md)
+`;function e(o){return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Introduction"}),`
+`,t.jsx(a,{children:i})]})}function h(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e()}export{h as default};
