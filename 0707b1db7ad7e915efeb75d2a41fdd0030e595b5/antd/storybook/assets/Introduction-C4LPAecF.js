d864b2c6a9253de143b428ba409caa4e58b36da4
evlmaistrenko <evlmaistrenko@users.noreply.github.com>
2025-06-11 08:24:31 +0000

    deploy: 0707b1db7ad7e915efeb75d2a41fdd0030e595b5

---
+++ 0707b1db7ad7e915efeb75d2a41fdd0030e595b5/antd/storybook/assets/Introduction-C4LPAecF.js
@@ -0,0 +1,24 @@
+import{j as n,b as s,c as a}from"./blocks-DNFsmE6z.js";import{useMDXComponents as r}from"./index-oSmXUL5b.js";import"./iframe-BBXgo6B6.js";const i=`# @evlmaistrenko/tools-antd
+
+[![NPM Version](https://img.shields.io/npm/v/%40evlmaistrenko%2Ftools-antd)](https://www.npmjs.com/package/@evlmaistrenko/tools-antd)
+
+React-components based on Ant Design.
+
+## Usage
+
+\`\`\`bash
+npm i @evlmaistrenko/tools-antd
+\`\`\`
+
+\`\`\`javascript
+import { TelegramOutlined } from "@evlmaistrenko/tools-antd";
+
+const telegramOutlined = <TelegramOutlined />;
+\`\`\`
+
+## Api docs
+
+- [Typedoc](https://evlmaistrenko.github.io/js-tools/antd/typedoc/)
+- [Storybook](https://evlmaistrenko.github.io/js-tools/antd/storybook/)
+`;function e(t){return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Introduction"}),`
+`,n.jsx(a,{children:i})]})}function p(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e()}export{p as default};
