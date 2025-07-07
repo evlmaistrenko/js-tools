993ba65bffbca70f815026d81f599a48ff03fb71
evlmaistrenko <evlmaistrenko@users.noreply.github.com>
2025-07-07 00:39:19 +0000

    deploy: 9d3f5ec1d7f98438ee592478bc8614760e547111

---
+++ 9d3f5ec1d7f98438ee592478bc8614760e547111/antd/storybook/assets/IconSelect.stories-BSCFhRbS.js
@@ -0,0 +1,13 @@
+import{j as e}from"./iframe-DrCYgfLS.js";import{I as a,F as n}from"./index-BuLHdxX-.js";import{R as r,a as s}from"./PieChartOutlined-DfLPCS6x.js";import"./index-ruu9K9Xr.js";import"./mediaQueryUtil-BnZLGvuw.js";import"./ExclamationCircleFilled-BjeT5jXM.js";import"./presets-DEv5GCtW.js";const f={title:"Components/DataEntry/IconSelect",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{options:{control:!1,table:{type:{summary:"(BaseOptionType & {icon?: ReactNode})[]"}}}},render:o=>e.jsx(n,{layout:"vertical",children:e.jsx(n.Item,{name:"icon-select",label:"Icon select",children:e.jsx(a,{...o,style:{...o.style,width:150}})})})},t={args:{options:[{label:"Option 1",value:"1",icon:e.jsx(r,{})},{label:"Option 2",value:"2",icon:e.jsx(s,{})}]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      label: "Option 1",
+      value: "1",
+      icon: <AreaChartOutlined />
+    }, {
+      label: "Option 2",
+      value: "2",
+      icon: <PieChartOutlined />
+    }]
+  }
+}`,...t.parameters?.docs?.source}}};const x=["Default"];export{t as Default,x as __namedExportsOrder,f as default};
