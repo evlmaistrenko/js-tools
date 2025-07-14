e50b5b1891dbaca6379af5f5d3d25c0089060b3a
evlmaistrenko <evlmaistrenko@users.noreply.github.com>
2025-07-14 00:38:27 +0000

    deploy: be38d10ac8097732d3986688668eef8c6a7d9a88

---
+++ be38d10ac8097732d3986688668eef8c6a7d9a88/antd/storybook/assets/Introduction-MBKUEm58.js
@@ -0,0 +1,26 @@
+import{j as t}from"./iframe-A4trVNTJ.js";import{useMDXComponents as s}from"./index-3A4Egcyl.js";import{M as a,c as r}from"./blocks-DsYKEx1g.js";const m=`# @evlmaistrenko/tools-antd
+
+[![NPM Version](https://img.shields.io/npm/v/%40evlmaistrenko%2Ftools-antd)](https://www.npmjs.com/package/@evlmaistrenko/tools-antd)
+
+React-components based on Ant Design.
+
+## Usage
+
+\`\`\`bash
+npm i @evlmaistrenko/tools-antd
+\`\`\`
+
+\`\`\`javascript
+import "@evlmaistrenko/tools-antd/index.css";
+import { TelegramOutlined } from "@evlmaistrenko/tools-antd";
+
+const telegramOutlined = <TelegramOutlined />;
+\`\`\`
+
+## Api docs
+
+- [Typedoc](https://evlmaistrenko.github.io/js-tools/antd/typedoc/)
+- [Storybook](https://evlmaistrenko.github.io/js-tools/antd/storybook/)
+- [CHANGELOG.md](./CHANGELOG.md)
+`;function e(n){return t.jsxs(t.Fragment,{children:[t.jsx(a,{title:"Introduction"}),`
+`,t.jsx(r,{children:m.replace("- [CHANGELOG.md](./CHANGELOG.md)","- [CHANGELOG.md](https://github.com/evlmaistrenko/js-tools/blob/master/packages/antd/CHANGELOG.md)")})]})}function p(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e()}export{p as default};
---
+++ be38d10ac8097732d3986688668eef8c6a7d9a88/antd/storybook/assets/WithOverlay.stories-BT0lbkgp.js
@@ -0,0 +1,9 @@
+import{j as r}from"./iframe-A4trVNTJ.js";import{W as t}from"./index-B4ClrUx8.js";const o={title:"Components/Utilities/WithOverlay",component:t,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{children:{table:{disable:!0}},overlayProps:{control:!1,table:{type:{summary:'Omit<HTMLAttributes<HTMLDivElement>, "children">'}}}}},e={args:{overlaid:!1,children:r.jsx("div",{style:{background:"#ececec",padding:32},children:"CONTENT"})}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    overlaid: false,
+    children: <div style={{
+      background: "#ececec",
+      padding: 32
+    }}>CONTENT</div>
+  }
+}`,...e.parameters?.docs?.source}}};const n=["Default"];export{e as Default,n as __namedExportsOrder,o as default};
