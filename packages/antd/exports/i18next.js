42ade38598d82918139014014ad222f1219f78c1
evlmaistrenko <72499922+evlmaistrenko@users.noreply.github.com>
2025-07-31 20:12:50 +0500

    feat: update i18next exports and enhance Application component with locale handling

--- packages/antd/exports/i18next.js
+++ packages/antd/exports/i18next.js
@@ -1,1 +1,1 @@
-export * from "../dist/i18next.js"
+export { i18next, i18nextResources } from "../dist/i18next.js"
--- packages/antd/src/next/client/application.tsx
+++ packages/antd/src/next/client/application.tsx
@@ -1,10 +1,48 @@
-import type { FC } from "react"
+import { type FC, useEffect, useState } from "react"
+
+import { usePathname, useRouter } from "next/navigation"
 
 import {
 	type ApplicationProps,
 	Application as Base,
 } from "../../components/application"
+import type {
+	ApplicationConfigBase,
+	ApplicationContextValue,
+} from "../../components/application/context"
 
-export const Application: FC<ApplicationProps> = (props) => {
-	return <Base {...props} />
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ""
+
+export const Application: FC<ApplicationProps<ApplicationConfigBase>> = (
+	props,
+) => {
+	const [ref, setRef] =
+		useState<ApplicationContextValue<ApplicationConfigBase> | null>()
+	const locale = ref?.config.values.locale
+	const router = useRouter()
+	const pathname = usePathname()
+	useEffect(() => {
+		if (!locale) return
+		const url = new URL(window.location.href)
+		const currentLocale = pathname
+			.replace(basePath, "")
+			.split("/")
+			.filter(Boolean)[0]
+		if (!["en-US", "kk-KZ", "ru-RU"].includes(currentLocale)) return
+		if (locale !== currentLocale) {
+			url.pathname = url.pathname.replace(
+				new RegExp(`/${currentLocale}(/|$)`),
+				`/${locale}$1`,
+			)
+			document.documentElement.lang = locale
+			router.replace(`${url.pathname}${url.search}${url.hash}`)
+		}
+	}, [locale, router, pathname])
+
+	return (
+		<Base
+			{...props}
+			ref={setRef}
+		/>
+	)
 }
