4b5ea120146e19d60043bb0c87a450b3f897bbec
evlmaistrenko <evlmaistrenko@users.noreply.github.com>
2025-08-01 07:41:32 +0000

    deploy: 2cd7c0fdcd0788b8cf6057eb32a6d26bd94dbd87

---
+++ 2cd7c0fdcd0788b8cf6057eb32a6d26bd94dbd87/antd/storybook/assets/IconSelect.stories-_9wQ1hlr.js
@@ -0,0 +1,13 @@
+import{j as e}from"./iframe-DMh57n2j.js";import{I as a,F as n}from"./index-vuG1UNgj.js";import{R as r,a as s}from"./PieChartOutlined-CR09nlyg.js";import"./index-BqbeDyAe.js";import"./DownOutlined-BlYGCqTD.js";import"./ExclamationCircleFilled--o7lBohW.js";import"./presets-DEv5GCtW.js";const f={title:"Components/DataEntry/IconSelect",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{options:{control:!1,table:{type:{summary:"(BaseOptionType & {icon?: ReactNode})[]"}}}},render:o=>e.jsx(n,{layout:"vertical",children:e.jsx(n.Item,{name:"icon-select",label:"Icon select",children:e.jsx(a,{...o,style:{...o.style,width:150}})})})},t={args:{options:[{label:"Option 1",value:"1",icon:e.jsx(r,{})},{label:"Option 2",value:"2",icon:e.jsx(s,{})}]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      label: "Option 1",
+      value: "1",
+      icon: <AreaChartOutlined />
+    }, {
+      label: "Option 2",
+      value: "2",
+      icon: <PieChartOutlined />
+    }]
+  }
+}`,...t.parameters?.docs?.source}}};const x=["Default"];export{t as Default,x as __namedExportsOrder,f as default};
---
+++ 2cd7c0fdcd0788b8cf6057eb32a6d26bd94dbd87/antd/storybook/assets/PieChartOutlined-CR09nlyg.js
@@ -0,0 +1,58 @@
+import{r as l,I as W,b as Y,e as A,a as q}from"./iframe-DMh57n2j.js";import{g as K,b as U}from"./presets-DEv5GCtW.js";var k={icon:{tag:"svg",attrs:{"fill-rule":"evenodd",viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M799.86 166.31c.02 0 .04.02.08.06l57.69 57.7c.04.03.05.05.06.08a.12.12 0 010 .06c0 .03-.02.05-.06.09L569.93 512l287.7 287.7c.04.04.05.06.06.09a.12.12 0 010 .07c0 .02-.02.04-.06.08l-57.7 57.69c-.03.04-.05.05-.07.06a.12.12 0 01-.07 0c-.03 0-.05-.02-.09-.06L512 569.93l-287.7 287.7c-.04.04-.06.05-.09.06a.12.12 0 01-.07 0c-.02 0-.04-.02-.08-.06l-57.69-57.7c-.04-.03-.05-.05-.06-.07a.12.12 0 010-.07c0-.03.02-.05.06-.09L454.07 512l-287.7-287.7c-.04-.04-.05-.06-.06-.09a.12.12 0 010-.07c0-.02.02-.04.06-.08l57.7-57.69c.03-.04.05-.05.07-.06a.12.12 0 01.07 0c.03 0 .05.02.09.06L512 454.07l287.7-287.7c.04-.04.06-.05.09-.06a.12.12 0 01.07 0z"}}]},name:"close",theme:"outlined"},F=function(n,t){return l.createElement(W,Y({},n,{ref:t,icon:k}))},$e=l.forwardRef(F);function xe(e){return e==null?null:typeof e=="object"&&!l.isValidElement(e)?e:{title:e}}var G={icon:{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M888 792H200V168c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v688c0 4.4 3.6 8 8 8h752c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm-616-64h536c4.4 0 8-3.6 8-8V284c0-7.2-8.7-10.7-13.7-5.7L592 488.6l-125.4-124a8.03 8.03 0 00-11.3 0l-189 189.6a7.87 7.87 0 00-2.3 5.6V720c0 4.4 3.6 8 8 8z"}}]},name:"area-chart",theme:"outlined"},J={icon:{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M864 518H506V160c0-4.4-3.6-8-8-8h-26a398.46 398.46 0 00-282.8 117.1 398.19 398.19 0 00-85.7 127.1A397.61 397.61 0 0072 552a398.46 398.46 0 00117.1 282.8c36.7 36.7 79.5 65.6 127.1 85.7A397.61 397.61 0 00472 952a398.46 398.46 0 00282.8-117.1c36.7-36.7 65.6-79.5 85.7-127.1A397.61 397.61 0 00872 552v-26c0-4.4-3.6-8-8-8zM705.7 787.8A331.59 331.59 0 01470.4 884c-88.1-.4-170.9-34.9-233.2-97.2C174.5 724.1 140 640.7 140 552c0-88.7 34.5-172.1 97.2-234.8 54.6-54.6 124.9-87.9 200.8-95.5V586h364.3c-7.7 76.3-41.3 147-96.6 201.8zM952 462.4l-2.6-28.2c-8.5-92.1-49.4-179-115.2-244.6A399.4 399.4 0 00589 74.6L560.7 72c-4.7-.4-8.7 3.2-8.7 7.9V464c0 4.4 3.6 8 8 8l384-1c4.7 0 8.4-4 8-8.6zm-332.2-58.2V147.6a332.24 332.24 0 01166.4 89.8c45.7 45.6 77 103.6 90 166.1l-256.4.7z"}}]},name:"pie-chart",theme:"outlined"};const P=l.createContext({});function X(){return!!(typeof window<"u"&&window.document&&window.document.createElement)}function Z(e,n){if(!e)return!1;if(e.contains)return e.contains(n);let t=n;for(;t;){if(t===e)return!0;t=t.parentNode}return!1}const E="data-rc-order",S="data-rc-priority",ee="rc-util-key",b=new Map;function j({mark:e}={}){return e?e.startsWith("data-")?e:`data-${e}`:ee}function O(e){return e.attachTo?e.attachTo:document.querySelector("head")||document.body}function ne(e){return e==="queue"?"prependQueue":e?"prepend":"append"}function R(e){return Array.from((b.get(e)||e).children).filter(n=>n.tagName==="STYLE")}function M(e,n={}){if(!X())return null;const{csp:t,prepend:r,priority:o=0}=n,a=ne(r),s=a==="prependQueue",i=document.createElement("style");i.setAttribute(E,a),s&&o&&i.setAttribute(S,`${o}`),t?.nonce&&(i.nonce=t?.nonce),i.innerHTML=e;const u=O(n),{firstChild:f}=u;if(r){if(s){const c=(n.styles||R(u)).filter(p=>{if(!["prepend","prependQueue"].includes(p.getAttribute(E)))return!1;const y=Number(p.getAttribute(S)||0);return o>=y});if(c.length)return u.insertBefore(i,c[c.length-1].nextSibling),i}u.insertBefore(i,f)}else u.appendChild(i);return i}function te(e,n={}){let{styles:t}=n;return t||=R(O(n)),t.find(r=>r.getAttribute(j(n))===e)}function re(e,n){const t=b.get(e);if(!t||!Z(document,t)){const r=M("",n),{parentNode:o}=r;b.set(e,o),e.removeChild(r)}}function oe(e,n,t={}){const r=O(t),o=R(r),a={...t,styles:o};re(r,a);const s=te(n,a);if(s)return a.csp?.nonce&&s.nonce!==a.csp?.nonce&&(s.nonce=a.csp?.nonce),s.innerHTML!==e&&(s.innerHTML=e),s;const i=M(e,a);return i.setAttribute(j(a),n),i}function z(e){return e?.getRootNode?.()}function ae(e){return z(e)instanceof ShadowRoot}function ie(e){return ae(e)?z(e):null}let w={};const ce=e=>{};function se(e,n){}function le(e,n){}function ue(){w={}}function L(e,n,t){!n&&!w[t]&&(e(!1,t),w[t]=!0)}function h(e,n){L(se,e,n)}function fe(e,n){L(le,e,n)}h.preMessage=ce;h.resetWarned=ue;h.noteOnce=fe;function de(e){return e.replace(/-(.)/g,(n,t)=>t.toUpperCase())}function me(e,n){h(e,`[@ant-design/icons] ${n}`)}function N(e){return typeof e=="object"&&typeof e.name=="string"&&typeof e.theme=="string"&&(typeof e.icon=="object"||typeof e.icon=="function")}function I(e={}){return Object.keys(e).reduce((n,t)=>{const r=e[t];switch(t){case"class":n.className=r,delete n.class;break;default:delete n[t],n[de(t)]=r}return n},{})}function v(e,n,t){return t?A.createElement(e.tag,{key:n,...I(e.attrs),...t},(e.children||[]).map((r,o)=>v(r,`${n}-${e.tag}-${o}`))):A.createElement(e.tag,{key:n,...I(e.attrs)},(e.children||[]).map((r,o)=>v(r,`${n}-${e.tag}-${o}`)))}function V(e){return K(e)[0]}function _(e){return e?Array.isArray(e)?e:[e]:[]}const ge=`
+.anticon {
+  display: inline-flex;
+  align-items: center;
+  color: inherit;
+  font-style: normal;
+  line-height: 0;
+  text-align: center;
+  text-transform: none;
+  vertical-align: -0.125em;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.anticon > * {
+  line-height: 1;
+}
+
+.anticon svg {
+  display: inline-block;
+}
+
+.anticon::before {
+  display: none;
+}
+
+.anticon .anticon-icon {
+  display: block;
+}
+
+.anticon[tabindex] {
+  cursor: pointer;
+}
+
+.anticon-spin::before,
+.anticon-spin {
+  display: inline-block;
+  -webkit-animation: loadingCircle 1s infinite linear;
+  animation: loadingCircle 1s infinite linear;
+}
+
+@-webkit-keyframes loadingCircle {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes loadingCircle {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+`,pe=e=>{const{csp:n,prefixCls:t,layer:r}=l.useContext(P);let o=ge;t&&(o=o.replace(/anticon/g,t)),r&&(o=`@layer ${r} {
+${o}
+}`),l.useEffect(()=>{const a=e.current,s=ie(a);oe(o,"@ant-design-icons",{prepend:!r,csp:n,attachTo:s})},[])},m={primaryColor:"#333",secondaryColor:"#E6E6E6",calculated:!1};function he({primaryColor:e,secondaryColor:n}){m.primaryColor=e,m.secondaryColor=n||V(e),m.calculated=!!n}function ye(){return{...m}}const d=e=>{const{icon:n,className:t,onClick:r,style:o,primaryColor:a,secondaryColor:s,...i}=e,u=l.useRef();let f=m;if(a&&(f={primaryColor:a,secondaryColor:s||V(a)}),pe(u),me(N(n),`icon should be icon definiton, but got ${n}`),!N(n))return null;let c=n;return c&&typeof c.icon=="function"&&(c={...c,icon:c.icon(f.primaryColor,f.secondaryColor)}),v(c.icon,`svg-${c.name}`,{className:t,onClick:r,style:o,"data-icon":c.name,width:"1em",height:"1em",fill:"currentColor","aria-hidden":"true",...i,ref:u})};d.displayName="IconReact";d.getTwoToneColors=ye;d.setTwoToneColors=he;function B(e){const[n,t]=_(e);return d.setTwoToneColors({primaryColor:n,secondaryColor:t})}function Ce(){const e=d.getTwoToneColors();return e.calculated?[e.primaryColor,e.secondaryColor]:e.primaryColor}function T(){return T=Object.assign?Object.assign.bind():function(e){for(var n=1;n<arguments.length;n++){var t=arguments[n];for(var r in t)Object.prototype.hasOwnProperty.call(t,r)&&(e[r]=t[r])}return e},T.apply(this,arguments)}B(U.primary);const g=l.forwardRef((e,n)=>{const{className:t,icon:r,spin:o,rotate:a,tabIndex:s,onClick:i,twoToneColor:u,...f}=e,{prefixCls:c="anticon",rootClassName:p}=l.useContext(P),y=q(p,c,{[`${c}-${r.name}`]:!!r.name,[`${c}-spin`]:!!o||r.name==="loading"},t);let C=s;C===void 0&&i&&(C=-1);const D=a?{msTransform:`rotate(${a}deg)`,transform:`rotate(${a}deg)`}:void 0,[H,Q]=_(u);return l.createElement("span",T({role:"img","aria-label":r.name},f,{ref:n,tabIndex:C,onClick:i,className:y}),l.createElement(d,{icon:r,primaryColor:H,secondaryColor:Q,style:D}))});g.displayName="AntdIcon";g.getTwoToneColor=Ce;g.setTwoToneColor=B;function $(){return $=Object.assign?Object.assign.bind():function(e){for(var n=1;n<arguments.length;n++){var t=arguments[n];for(var r in t)Object.prototype.hasOwnProperty.call(t,r)&&(e[r]=t[r])}return e},$.apply(this,arguments)}const be=(e,n)=>l.createElement(g,$({},e,{ref:n,icon:G})),Oe=l.forwardRef(be);function x(){return x=Object.assign?Object.assign.bind():function(e){for(var n=1;n<arguments.length;n++){var t=arguments[n];for(var r in t)Object.prototype.hasOwnProperty.call(t,r)&&(e[r]=t[r])}return e},x.apply(this,arguments)}const we=(e,n)=>l.createElement(g,x({},e,{ref:n,icon:J})),Re=l.forwardRef(we);export{g as I,Oe as R,Re as a,$e as b,xe as c};
