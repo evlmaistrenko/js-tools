e0844a3c57ec9bb34fde5456be3da0f30dea6aae
evlmaistrenko <evlmaistrenko@users.noreply.github.com>
2025-08-02 22:16:59 +0000

    deploy: 04154ebb5ff425e7d2091df75e465d289cc8cf0a

---
+++ 04154ebb5ff425e7d2091df75e465d289cc8cf0a/antd/storybook/assets/Introduction-DarJOfVk.js
@@ -0,0 +1,29 @@
+import{j as n}from"./iframe-D3dxFt6v.js";import{useMDXComponents as s}from"./index-Bt6SAnG5.js";import{M as a,c as r}from"./blocks-BEIbranN.js";const i=`# @evlmaistrenko/tools-antd
+
+[![NPM Version](https://img.shields.io/npm/v/%40evlmaistrenko%2Ftools-antd)](https://www.npmjs.com/package/@evlmaistrenko/tools-antd)
+
+<div align="center">
+  <img src="./logo.svg" alt="Логотип" width="460" />
+</div>
+
+React-components based on Ant Design.
+
+## Usage
+
+\`\`\`bash
+npm i @evlmaistrenko/tools-antd
+\`\`\`
+
+\`\`\`javascript
+import { TelegramOutlined } from "@evlmaistrenko/tools-antd";
+
+const telegramOutlined = <TelegramOutlined />;
+\`\`\`
+
+## Api docs
+
+- [Typedoc](https://evlmaistrenko.github.io/js-tools/antd/typedoc/)
+- [Storybook](https://evlmaistrenko.github.io/js-tools/antd/storybook/)
+- [CHANGELOG.md](./CHANGELOG.md)
+`;function e(t){return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Introduction"}),`
+`,n.jsx(r,{children:i.replace("- [CHANGELOG.md](./CHANGELOG.md)","- [CHANGELOG.md](https://github.com/evlmaistrenko/js-tools/blob/master/packages/antd/CHANGELOG.md)")})]})}function c(t={}){const{wrapper:o}={...s(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e()}export{c as default};
